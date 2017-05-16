61fb3dbcc892d679d9f7ff6b47c4b7f9149d8d45
Daniel Critchfield <Daniel Critchfield>
2017-05-16 12:15:48 -0400

    made a quick correction to method description, odd typescript syntax

--- ControllerTemplate/Controller.ts
+++ ControllerTemplate/Controller.ts
@@ -5,8 +5,8 @@ class Controller {
     }
 
     // force screen to be in the orientation described by the string parameter
-    forceScreenOrientation(orientation:string) {
-        
+    public forceScreenOrientation = (orientation:string): void => {
+
     }
 
     
