1539820eb3f3db01f6cb60a6dfad2f247f158dc4
Daniel Critchfield <Daniel Critchfield>
2017-05-16 18:22:35 -0400

    added a few more empty functions any controller should need

--- ControllerTemplate/Controller.ts
+++ ControllerTemplate/Controller.ts
@@ -5,9 +5,48 @@ class Controller {
     }
 
     // force screen to be in the orientation described by the string parameter
-    public forceScreenOrientation = (orientation:string): void => {
+    private forceScreenOrientation = (orientation:string) => {
 
     }
 
-    
+    // called to clear the canvas after something is drawn or needs to be cleared
+    private resetCanvas = (e) => {
+
+    }
+
+    // draws the joystick
+    private drawJoyStick = () => {
+
+    }
+
+    // draws the main action pad
+    private drawActionPad = () => {
+
+    }
+
+    // Returns an object with xdir and ydir that has the direction between
+    // -1 and 1 in each position
+    public getAnalogueDirection = ():any => {
+        
+    }
+
+    // Returns an object with xdir and ydir that has either -1, 1, or 0 for
+    // each value
+    public getDigDirection = ():any => {
+
+    }
+
+    private dPadObject = (SelectedCanvas, startTouch, moveTouch, endTouch):void => {
+
+    }
+
+    private touchObject = (SelectedCanvas, tapFunction, doubleTapFunction, swipeRFunction, swipeLFunction, swipeUFunction, swipeDFunction):void => {
+
+    }
+
+    // the parameter is the id of the canvas object you want to setup from the html document
+    // returns the canvas object setup
+    private setupCanvas = (canvasID:string):any => {
+
+    }
 }
