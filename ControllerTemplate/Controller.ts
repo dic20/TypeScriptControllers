454d63fd63f000da8117d958c6b499a503654f7f
Daniel Critchfield <Daniel Critchfield>
2017-05-16 12:10:10 -0400

    added to the Controller.ts template a few comments and a function definition

--- ControllerTemplate/Controller.ts
+++ ControllerTemplate/Controller.ts
@@ -1,5 +1,13 @@
 class Controller {
+    // should be game specific controllers, but this will be a simple template to use
     constructor(tapFunction:Function, doubleTapFunctin:Function, swipeRFunction:Function, swipeLFunction:Function, swipeUFunction:Function, swipeDFunction:Function, touchStart:Function, touchMove:Function) {
 
     }
+
+    // force screen to be in the orientation described by the string parameter
+    forceScreenOrientation(orientation:string) {
+        
+    }
+
+    
 }
